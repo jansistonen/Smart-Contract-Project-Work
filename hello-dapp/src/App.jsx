602644795a3bd8ec656bd5aeefb2d1965b4cde78
vehvi <anttivehvilainen123@gmail.com>
2025-05-21 15:54:22 +0300

    Added network and connected wallet fields. Added alert if MetaMask is not installed

--- hello-dapp/src/App.jsx
+++ hello-dapp/src/App.jsx
@@ -5,12 +5,36 @@ import { CONTRACT_ABI } from './contract';
 function App() {
   const [contractAddress, setContractAddress] = useState('');
   const [contract, setContract] = useState(null);
+  const [network, setNetwork] = useState(null);
+  const [walletAddress, setWalletAddress] = useState('');
   const [currentMessage, setCurrentMessage] = useState('');
   const [newMessage, setNewMessage] = useState('');
   const [status, setStatus] = useState('');
 
   // Tarkista osoitteen muoto
   const isValidAddress = (addr) => /^0x[a-fA-F0-9]{40}$/.test(addr);
+
+   // Ladataan wallet address ja network heti kun sovellus avautuu
+  useEffect(() => {
+    const loadWalletAndNetwork = async () => {
+      if (!window.ethereum) {
+        alert("Please install MetaMask!");
+        return;
+      }
+      try {
+        const provider = new BrowserProvider(window.ethereum);
+        const signer = await provider.getSigner();
+        const network = await provider.getNetwork();
+
+        setWalletAddress(await signer.getAddress());
+        setNetwork(network);
+      } catch (error) {
+        console.error("Error loading wallet/network:", error);
+        setStatus('Failed to load wallet/network');
+    }
+  };
+  loadWalletAndNetwork();
+}, []);
 
   // Luo sopimus-olio kun osoite vaihtuu
   useEffect(() => {
@@ -25,6 +49,7 @@ function App() {
         const instance = new Contract(contractAddress, CONTRACT_ABI, signer);
         setContract(instance);
         setStatus('Contract loaded');
+        
       } catch (error) {
         console.error("Error creating contract:", error);
         setStatus('Failed to connect to contract');
@@ -64,6 +89,19 @@ function App() {
     <div style={{ padding: '2rem', maxWidth: '600px', margin: 'auto', fontFamily: 'sans-serif' }}>
       <h1>Hello dApp</h1>
 
+      {network?.name ? (
+          <p><strong>Network: </strong> {network.name}</p>
+      ) : (
+          <p><strong>Network: </strong> Unidentified</p>
+      )}
+
+      {walletAddress ? (
+          <p><strong>Connected Wallet:</strong> {walletAddress}</p>
+      ) : (
+          <p><strong>Connected Wallet:</strong> MetaMask wallet not found</p>
+      )}
+      <p><strong>Current message:</strong> {currentMessage}</p>
+
       <label>Smart Contract Address:</label>
       <input
         type="text"
@@ -74,8 +112,6 @@ function App() {
       />
 
       <button onClick={getMessage} disabled={!contract}>Get Message</button>
-
-      <p><strong>Current message:</strong> {currentMessage}</p>
 
       <input
         type="text"
